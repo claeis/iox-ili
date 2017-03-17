afbe0707b4df97eb2fe0bb9e62e3cac3cda1f9d3
claeis <claeis@users.noreply.github.com>
2017-03-17 17:15:19 +0100

    removed experiment/not a real test

--- jtsext/src/test/java/ch/interlis/iom_j/itf/impl/jtsext/geom/KleinpTest.java
+++ jtsext/src/test/java/ch/interlis/iom_j/itf/impl/jtsext/geom/KleinpTest.java
@@ -10,15 +10,6 @@ public class KleinpTest {
 	static final double EPS=0.00000001;
 
 	@Test
-	public void test() {
-		Coordinate p0=new Coordinate(0.0,0.0);
-		Coordinate p1=new Coordinate(0.7755148819837229, 6.8213473701943865);
-		Coordinate ret=CompoundCurve.calcKleinp(p0, p1, 3.4326448722900285, 0.6076121683173152);
-		System.out.println(ret);
-		assertEquals(ret.x,5.0,EPS);
-		assertEquals(ret.y,1.0,EPS);
-	}
-	@Test
 	public void startLinks() {
 		Coordinate p0=new Coordinate(0.0,0.0);
 		Coordinate p1=new Coordinate(0.0,5.0);
