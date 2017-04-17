6c008d10883ab28982d8928e71c8cf8b0a8aede3
Stefan Henrich <github@stehen.ch>
2017-04-17 23:10:21 +0200

    set constraint - iteration not updated
    main object has to be set in every iteration

--- src/main/java/ch/interlis/iox_j/validator/Validator.java
+++ src/main/java/ch/interlis/iox_j/validator/Validator.java
@@ -664,6 +664,7 @@ public class Validator implements ch.interlis.iox.IoxValidator {
 				for(String oid:objs){
 					allObjIterator=objs.iterator();
 					IomObject iomObj=objectPool.getObject(oid, null, null);
+					setCurrentMainObj(iomObj);
 					Evaluable condition = (Evaluable) setConstraintObj.getCondition();
 					Value constraintValue = evaluateExpression(iomObj, condition);
 					if (constraintValue.isNotYetImplemented()){
