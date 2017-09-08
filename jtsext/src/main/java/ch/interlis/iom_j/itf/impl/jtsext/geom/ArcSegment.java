e07f30625d3b202be28827b53f552b9412da7602
claeis <claeis@users.noreply.github.com>
2017-09-08 15:41:14 +0200

    fixed coordinates calculation in ArcSegment if full circle

--- jtsext/src/main/java/ch/interlis/iom_j/itf/impl/jtsext/geom/ArcSegment.java
+++ jtsext/src/main/java/ch/interlis/iom_j/itf/impl/jtsext/geom/ArcSegment.java
@@ -84,20 +84,14 @@ public class ArcSegment extends CurveSegment {
 			double a=CurveSegment.dist(pt1_re,pt1_ho,arcPt_re,arcPt_ho);
 			// Distanz zwischen Zwischenpunkt und Bogenendpunkt 
 			double b=CurveSegment.dist(arcPt_re,arcPt_ho,pt2_re,pt2_ho);
+			
+			getCenterPoint();
 
-			// Zwischenpunkte erzeugen, so dass maximale Pfeilhoehe nicht 
-			// ueberschritten wird
-			// Distanz zwischen Bogenanfanspunkt und Bogenendpunkt 
-			double c=CurveSegment.dist(pt1_re,pt1_ho,pt2_re,pt2_ho);
-			// Radius bestimmen
-			double s=(a+b+c)/2.0;
-			double ds=Math.atan2(pt2_re-arcPt_re,pt2_ho-arcPt_ho)-Math.atan2(pt1_re-arcPt_re,pt1_ho-arcPt_ho);
-			double rSign=(Math.sin(ds)>0.0)?-1.0:1.0;
-			double r=a*b*c/4.0/Math.sqrt(s*(s-a)*(s-b)*(s-c))*rSign;
+			double r=radius;
 			// Kreismittelpunkt
-			double thetaM=Math.atan2(arcPt_re-pt1_re,arcPt_ho-pt1_ho)+Math.acos(a/2.0/r);
-			double reM=pt1_re+r*Math.sin(thetaM);
-			double hoM=pt1_ho+r*Math.cos(thetaM);
+			double thetaM=deta;
+			double reM=centerPoint.x;
+			double hoM=centerPoint.y;
 
 			// mindest Winkelschrittweite
 			double theta=2*Math.acos(1-p/Math.abs(r));
