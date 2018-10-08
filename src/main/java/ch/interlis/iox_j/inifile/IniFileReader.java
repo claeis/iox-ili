8b4d894bcc1c4fd6ecb1e39ecf0fa27265a33126
alideniz1986 <ali.dnz25@gmail.com>
2018-10-08 16:54:37 +0200

    switch config file reader to pure ini file syntax

--- src/main/java/ch/interlis/iox_j/inifile/IniFileReader.java
+++ src/main/java/ch/interlis/iox_j/inifile/IniFileReader.java
@@ -1,77 +1,126 @@
 package ch.interlis.iox_j.inifile;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.Map;
-import java.util.Properties;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 import ch.interlis.iox_j.validator.ValidationConfig;
 
 public class IniFileReader {
-    static public ValidationConfig readFile(java.io.File file) throws FileNotFoundException {
+    static public ValidationConfig readFile(java.io.File file) throws Exception {
         ValidationConfig config = null;
-        try {
-            config = new ValidationConfig();
-            mergeIniFile(config, file);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        config = new ValidationConfig();
+        mergeIniFile(config, file);
         return config;
     }
 
     private static void mergeIniFile(ValidationConfig config, File file) throws Exception {
+        BufferedReader br = null;
         try {
-            int i = 0;
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
             String key = null;
             String value = null;
             String header = null;
-            Properties properties = new Properties();
-            properties.load(new FileInputStream(file));
-            for (Map.Entry<Object, Object> e : properties.entrySet()) {
-                i++;
-                if (e.getKey().toString().startsWith("[") && e.getKey().toString().endsWith("]")) {
-                    header = getHeaderValue(e.getKey().toString().substring(1, e.getKey().toString().length() - 1));
-                } else if (!(e.getKey().toString().startsWith("[")) && !(e.getKey().toString().endsWith("]"))) {
-                    key = getKeyValue((String) e.getKey());
-                    value = getValue((String) e.getValue());
-                } else {
-                    throw new Exception("There is an error at the Header format! ([) or (]) missing!");
+            int numberOfLine = 0;
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                String lineWithoutCommnet = removeComments(line);
+                if (lineWithoutCommnet.isEmpty()) {
+                    numberOfLine++;
+                    continue;
                 }
-                if (i == properties.size()) {
+                if (lineWithoutCommnet.startsWith("[")) {
+                    if (isValidHeader(lineWithoutCommnet)) {
+                        header = getHeaderValue(lineWithoutCommnet.substring(1, lineWithoutCommnet.length() - 1));
+                    } else {
+                        throw new Exception(
+                                "\"There is an error at the Header format! ([) or (]) missing! Line number: "
+                                        + numberOfLine);
+                    }
+
+                } else {
+                    String[] keyAndValue = getKeyAndValue(lineWithoutCommnet);
+                    if (keyAndValue != null) {
+                        key = keyAndValue[0];
+                        value = keyAndValue[1];
+                    } else {
+                        throw new Exception("\"There is an error at the Item format! (=) must be sein! Line number: "
+                                + numberOfLine);
+                    }
+                }
+
+                if (header != null && key != null && value != null) {
                     config.setConfigValue(header, key, value);
+                    header = key = value = null;
+                }
+                numberOfLine++;
+            }
+        } catch (UnsupportedEncodingException e) {
+            throw new Exception(e.getMessage());
+        } catch (FileNotFoundException e) {
+            throw new Exception(e.getMessage());
+        } catch (IOException e) {
+            throw new Exception(e.getMessage());
+        } finally {
+            if (br != null) {
+                br.close();
+            }
+
+        }
+    }
+
+    private static String stripQuotes(String line) {
+        return (line.startsWith("\"") && line.endsWith("\"")) == true ? line.substring(1, line.length() - 1) : line;
+    }
+
+    private static String[] getKeyAndValue(String line) {
+        String values[] = line.split("=");
+        values[0] = stripQuotes(values[0]);
+        values[1] = stripQuotes(values[1]);
+        return values;
+    }
+
+    private static String getHeaderValue(String headerValue) {
+        return (headerValue.startsWith("\"") && headerValue.endsWith("\"")) == true
+                ? headerValue.substring(1, headerValue.length() - 1)
+                : headerValue;
+    }
+
+    private static String removeComments(String currentLine) {
+        StringBuilder text = new StringBuilder();
+        int numberOfquotes = 0;
+        int numberOfBrackets = 0;
+        for (int i = 0; i < currentLine.length(); i++) {
+            char c = currentLine.charAt(i);
+            if (c == '#' && i == 0) {
+                break;
+            }
+            if (c == '[' || c == ']') {
+                numberOfBrackets++;
+            }
+            if (c == '"') {
+                numberOfquotes++;
+            }
+            if (numberOfBrackets > 0) {
+                if ((numberOfBrackets % 2) == 0 && c == '#') {
+                    break;
                 }
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+
+            if ((numberOfquotes % 2) == 0 && c == '#') {
+                break;
+            }
+
+            text.append(c);
         }
+        return text.toString().trim();
     }
 
-    private static String getValue(String value) {
-        return stripQuotes(splitComment(value));
-    }
-
-    private static String getKeyValue(String value) {
-        return stripQuotes(value);
-    }
-
-    private static String getHeaderValue(String value) {
-        value = stripQuotes(splitComment(value));
-        return value;
-    }
-
-    private static String splitComment(String value) {
-        if (value.contains("#")) {
-            String[] newValue = value.split("#");
-            value = newValue[0].trim();
-        }
-        return value;
-    }
-
-    private static String stripQuotes(String value) {
-        if (value.startsWith("\"") && value.endsWith("\"")) {
-            value = value.substring(1, value.length() - 1);
-        } 
-        return value;
+    private static boolean isValidHeader(String line) {
+        return line.startsWith("[") == true && line.endsWith("]") == true ? true : false;
     }
 }
--- src/test/java/ch/interlis/iox_j/inifile/IniFileReaderTest.java
+++ src/test/java/ch/interlis/iox_j/inifile/IniFileReaderTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import ch.interlis.iox_j.validator.ValidationConfig;
@@ -15,6 +14,8 @@ public class IniFileReaderTest {
         ValidationConfig ini = IniFileReader.readFile(new File("src/test/data/inifile/comments.ini"));
         assertNotNull(ini.getConfigParams("Beispiel1.Bodenbedeckung.Gebaeude"));
         assertEquals("Assekuranz-Nr {AssNr}", ini.getConfigValue("Beispiel1.Bodenbedeckung.Gebaeude", "keymsg.de"));
+        assertNotNull(ini.getConfigParams("Beispiel1#Bodenbedeckung.Gebaeude2"));
+        assertEquals("Assekuranz-Nr2 #AssNr#", ini.getConfigValue("Beispiel1#Bodenbedeckung.Gebaeude2", "keymsg#de"));
     }
 
     @Test
@@ -29,12 +30,6 @@ public class IniFileReaderTest {
         ValidationConfig ini = IniFileReader.readFile(new File("src/test/data/inifile/noQuotes.ini"));
         assertNotNull(ini.getConfigParams("Beispiel1.Bodenbedeckung.Gebaeude"));
         assertEquals("Assekuranz-Nr {AssNr}", ini.getConfigValue("Beispiel1.Bodenbedeckung.Gebaeude", "keymsg.de"));
-    }
-    
-    @Test
-    public void headerWithError() throws Exception {
-        ValidationConfig ini = IniFileReader.readFile(new File("src/test/data/inifile/headerWithError.ini"));
-        assertEquals(null, ini.getConfigParams("Beispiel1.Bodenbedeckung.Gebaeude"));
-    }    
+    }   
 
 }
