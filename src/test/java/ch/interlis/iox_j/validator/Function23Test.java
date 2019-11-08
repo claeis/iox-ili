32a4e5fee0bad8e807268df5842953f162874c17
claeis <ce@eisenhutinformatik.ch>
2019-11-08 10:53:42 +0100

    fix objectCount(UNDEFINED) (claeis/ilivalidator#203)

--- src/test/java/ch/interlis/iox_j/validator/Function23Test.java
+++ src/test/java/ch/interlis/iox_j/validator/Function23Test.java
@@ -671,9 +671,9 @@ public class Function23Test {
 		assertTrue(logger.getErrs().size()==0);
 	}
 	
-	// Es wird getestet ob eine Fehlermeldung ausgegeben wird, wenn bei der Funktion: objectCount(Role) die Anzahl der Rollen welche von S zu T referenzieren, 2 ist.
+	// Es wird getestet ob keine Fehlermeldung ausgegeben wird, wenn bei der Funktion: objectCount(Role) die Anzahl der Objekte welche von S zu T via Role referenzieren, 2 ist.
 	@Test
-	public void objectCountRole_Ok(){
+	public void objectCount_2Object_Ok(){
 		// erstes S->T
 		Iom_jObject iomObjS1=new Iom_jObject(ILI_CLASSS, OBJ_OID1);
 		Iom_jObject iomObjT1=new Iom_jObject(ILI_CLASST, OBJ_OID3);
@@ -1405,16 +1405,15 @@ public class Function23Test {
 		assertEquals("Set Constraint Function23.Topic.ClassQ.Constraint1 is not true.", logger.getErrs().get(0).getEventMsg());
 	}
 	
-	// Es wird getestet ob eine Fehlermeldung ausgegeben wird, wenn bei der Funktion: objectCount(Role), die Anzahl der Rollen von KlasseS nach KlasseT nicht stimmt.
+    // Es wird getestet ob eine Fehlermeldung ausgegeben wird, wenn bei der Funktion: objectCount(Role) die Anzahl der Objekte welche von S zu T via Role referenzieren 1!=2 ist.
 	@Test
-	//@Ignore("review model")
-	public void objectCountRole_Fail(){
+	public void objectCount_1Object_Fail(){
 		// erstes S->T
 		Iom_jObject iomObjS1=new Iom_jObject(ILI_CLASSS, OBJ_OID1);
 		Iom_jObject iomObjT1=new Iom_jObject(ILI_CLASST, OBJ_OID3);
 		Iom_jObject iomObjST1=new Iom_jObject(ILI_ASSOC_ST1, null);
-		iomObjST1.addattrobj(ILI_ASSOC_ST1_S1, "REF").setobjectrefoid(OBJ_OID1);
-		iomObjST1.addattrobj(ILI_ASSOC_ST1_T1, "REF").setobjectrefoid(OBJ_OID3);
+		iomObjST1.addattrobj(ILI_ASSOC_ST1_S1, "REF").setobjectrefoid(iomObjS1.getobjectoid());
+		iomObjST1.addattrobj(ILI_ASSOC_ST1_T1, "REF").setobjectrefoid(iomObjT1.getobjectoid());
 		ValidationConfig modelConfig=new ValidationConfig();
 		LogCollector logger=new LogCollector();
 		LogEventFactory errFactory=new LogEventFactory();
@@ -1431,6 +1430,25 @@ public class Function23Test {
 		assertTrue(logger.getErrs().size()==1);
 		assertEquals("Mandatory Constraint Function23.Topic.ClassS.Constraint1 is not true.", logger.getErrs().get(0).getEventMsg());
 	}
+    // Es wird getestet ob eine Fehlermeldung ausgegeben wird, wenn bei der Funktion: objectCount(Role) die Anzahl der Objekte welche von S zu T via Role referenzieren 1!=2 ist.
+    @Test
+    public void objectCount_0Object_Fail(){
+        // erstes S->T
+        Iom_jObject iomObjS1=new Iom_jObject(ILI_CLASSS, OBJ_OID1);
+        ValidationConfig modelConfig=new ValidationConfig();
+        LogCollector logger=new LogCollector();
+        LogEventFactory errFactory=new LogEventFactory();
+        Settings settings=new Settings();
+        Validator validator=new Validator(td, modelConfig,logger,errFactory,settings);
+        validator.validate(new StartTransferEvent());
+        validator.validate(new StartBasketEvent(ILI_TOPIC,BID1));
+        validator.validate(new ObjectEvent(iomObjS1));
+        validator.validate(new EndBasketEvent());
+        validator.validate(new EndTransferEvent());
+        // Asserts
+        assertTrue(logger.getErrs().size()==1);
+        assertEquals("Mandatory Constraint Function23.Topic.ClassS.Constraint1 is not true.", logger.getErrs().get(0).getEventMsg());
+    }
 	
 	
     @Test
