c6ce096302094c3fb998b4cd854f22538b14964a
Philipp Luethi <philipp.luethi@geowerkstatt.ch>
2022-01-25 18:29:57 +0100

    Remove unused code & mark deprecated functions

--- src/main/java/ch/interlis/iox_j/wkb/Iox2wkb.java
+++ src/main/java/ch/interlis/iox_j/wkb/Iox2wkb.java
@@ -23,18 +23,14 @@
 package ch.interlis.iox_j.wkb;
 
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.vividsolutions.jts.geom.Coordinate;
 
-import ch.ehi.basics.logging.EhiLogger;
 import ch.interlis.iom.IomConstants;
 import ch.interlis.iom.IomObject;
-import ch.interlis.iom_j.itf.impl.hrg.HrgUtility;
 import ch.interlis.iom_j.itf.impl.jtsext.geom.ArcSegment;
-import ch.interlis.iom_j.itf.impl.jtsext.geom.CurveSegment;
 import ch.interlis.iox_j.jts.Iox2jtsException;
 
 /** Utility to convert from INTERLIS to WKB geometry.
@@ -81,9 +77,8 @@ import ch.interlis.iox_j.jts.Iox2jtsException;
 public class Iox2wkb {
     private int outputDimension = 2;
     private ByteArrayOutputStream os = null;
-    private boolean asEWKB = true; 
-	// utility, no instances
-	private Iox2wkb(){}
+    private boolean asEWKB = true;
+
     public Iox2wkb(int outputDimension) {
         this(outputDimension, java.nio.ByteOrder.BIG_ENDIAN, true);
     }
@@ -94,7 +89,7 @@ public class Iox2wkb {
         this.outputDimension = outputDimension;
         this.asEWKB=asEWKB;
         os = new ByteArrayOutputStream(byteOrder);
-        
+
         if (outputDimension < 2 || outputDimension > 3)
           throw new IllegalArgumentException("Output dimension must be 2 or 3");
     }
@@ -212,123 +207,6 @@ public class Iox2wkb {
 			return new Coordinate(arcPt_re, arcPt_ho);
 	}
 
-	private static void arc2JTS(com.vividsolutions.jts.geom.CoordinateList ret,IomObject value,double p)
-	throws Iox2wkbException
-	{
-		if(value!=null){
-			String c1=value.getattrvalue("C1");
-			String c2=value.getattrvalue("C2");
-			String c3=value.getattrvalue("C3");
-
-			double pt2_re;
-			try{
-				pt2_re = Double.parseDouble(c1);
-			}catch(Exception ex){
-				throw new Iox2wkbException("failed to read C1 <"+c1+">",ex);
-			}
-			double pt2_ho;
-			try{
-				pt2_ho = Double.parseDouble(c2);
-			}catch(Exception ex){
-				throw new Iox2wkbException("failed to read C2 <"+c2+">",ex);
-			}
-			Coordinate arcSupport = arcSupportingCoord2JTS(value);
-			if(p==0.0){
-				ret.add(arcSupport);
-				ret.add(new com.vividsolutions.jts.geom.Coordinate(pt2_re, pt2_ho));
-				return;
-			}
-			int lastCoord=ret.size();
-			com.vividsolutions.jts.geom.Coordinate p1=null;
-			p1=ret.getCoordinate(lastCoord-1);
-			ArcSegment arc=new ArcSegment(p1, arcSupport,new Coordinate(pt2_re, pt2_ho),p);
-			Coordinate[] coords = arc.getCoordinates();
-			for(int i=1;i<coords.length;i++) {
-	            ret.add(coords[i]);
-			}
-		}
-	}
-	private int arc2JTS(IomObject startPt,IomObject value,double p)
-	throws Iox2wkbException
-	{
-		if(value==null){
-			return 0;
-		}
-			int pointc=0;
-			String c1=value.getattrvalue("C1");
-			String c2=value.getattrvalue("C2");
-			String c3=value.getattrvalue("C3");
-			String a1=value.getattrvalue("A1");
-			String a2=value.getattrvalue("A2");
-			double pt2_re;
-			try{
-				pt2_re = Double.parseDouble(c1);
-			}catch(Exception ex){
-				throw new Iox2wkbException("failed to read C1 <"+c1+">",ex);
-			}
-			double pt2_ho;
-			try{
-				pt2_ho = Double.parseDouble(c2);
-			}catch(Exception ex){
-				throw new Iox2wkbException("failed to read C2 <"+c2+">",ex);
-			}
-			double pt2_z=0.0;
-			if(c3!=null){
-				try{
-					pt2_z = Double.parseDouble(c3);
-				}catch(Exception ex){
-					throw new Iox2wkbException("failed to read C3 <"+c3+">",ex);
-				}
-			}
-			double arcPt_re;
-			try{
-				arcPt_re = Double.parseDouble(a1);
-			}catch(Exception ex){
-				throw new Iox2wkbException("failed to read A1 <"+a1+">",ex);
-			}
-			double arcPt_ho;
-			try{
-				arcPt_ho = Double.parseDouble(a2);
-			}catch(Exception ex){
-				throw new Iox2wkbException("failed to read A2 <"+a2+">",ex);
-			}
-			if(p==0.0){
-				throw new Iox2wkbException("illegal p");
-			}
-
-			String p1c1=startPt.getattrvalue("C1");
-			String p1c2=startPt.getattrvalue("C2");
-			String p1c3=startPt.getattrvalue("C3");
-			double pt1_re;
-			try{
-				pt1_re = Double.parseDouble(p1c1);
-			}catch(Exception ex){
-				throw new Iox2wkbException("failed to read C1 <"+p1c1+">",ex);
-			}
-			double pt1_ho;
-			try{
-				pt1_ho = Double.parseDouble(p1c2);
-			}catch(Exception ex){
-				throw new Iox2wkbException("failed to read C2 <"+p1c2+">",ex);
-			}
-			double pt1_z=0.0;
-			if(p1c3!=null){
-				try{
-					pt1_z = Double.parseDouble(p1c3);
-				}catch(Exception ex){
-					throw new Iox2wkbException("failed to read C3 <"+p1c3+">",ex);
-				}
-			}
-
-            ArcSegment arc=new ArcSegment(new Coordinate(pt1_re,pt1_ho), new Coordinate(arcPt_re, arcPt_ho),new Coordinate(pt2_re, pt2_ho),p);
-            Coordinate[] coords = arc.getCoordinates();
-            for(int i=1;i<coords.length;i++) {
-                writeCoord(coords[i].x,coords[i].y,0.0);
-                pointc++;
-            }
-			
-			return pointc;
-	}
 	/** Converts a COORD to a JTS Coordinate.
 	 * @param value INTERLIS COORD structure.
 	 * @return JTS Coordinate.
@@ -500,65 +378,13 @@ public class Iox2wkb {
 		return result;
 	}
 
-	private static com.vividsolutions.jts.geom.CoordinateList polyline2coordlist(IomObject polylineObj,boolean isSurfaceOrArea,double p)
-	throws Iox2wkbException
-	{
-		if(polylineObj==null){
-			return null;
-		}
-		com.vividsolutions.jts.geom.CoordinateList ret=new com.vividsolutions.jts.geom.CoordinateList();
-		// is POLYLINE?
-		if(isSurfaceOrArea){
-			IomObject lineattr=polylineObj.getattrobj("lineattr",0);
-			if(lineattr!=null){
-				//writeAttrs(out,lineattr);
-				throw new Iox2wkbException("Lineattributes not supported");							
-			}
-		}
-		boolean clipped=polylineObj.getobjectconsistency()==IomConstants.IOM_INCOMPLETE;
-		if(clipped){
-			throw new Iox2wkbException("clipped polyline not supported");
-		}
-		for(int sequencei=0;sequencei<polylineObj.getattrvaluecount("sequence");sequencei++){
-			if(clipped){
-				//out.startElement(tags::get_CLIPPED(),0,0);
-			}else{
-				// an unclipped polyline should have only one sequence element
-				if(sequencei>0){
-					throw new Iox2wkbException("unclipped polyline with multi 'sequence' elements");
-				}
-			}
-			IomObject sequence=polylineObj.getattrobj("sequence",sequencei);
-			for(int segmenti=0;segmenti<sequence.getattrvaluecount("segment");segmenti++){
-				IomObject segment=sequence.getattrobj("segment",segmenti);
-				//EhiLogger.debug("segmenttag "+segment.getobjecttag());
-				if(segment.getobjecttag().equals("COORD")){
-					// COORD
-					ret.add(coord2JTS(segment));
-				}else if(segment.getobjecttag().equals("ARC")){
-					// ARC
-					arc2JTS(ret,segment,p);
-				}else{
-					// custum line form
-					throw new Iox2wkbException("custom line form not supported");
-					//out.startElement(segment->getTag(),0,0);
-					//writeAttrs(out,segment);
-					//out.endElement(/*segment*/);
-				}
-
-			}
-			if(clipped){
-				//out.endElement(/*CLIPPED*/);
-			}
-		}
-		return ret;
-	}
 	/** Converts a SURFACE to a JTS Polygon.
 	 * @param obj INTERLIS SURFACE structure
 	 * @param strokeP maximum stroke to use when removing ARCs
 	 * @return JTS Polygon
 	 * @throws Iox2wkbException
 	 */
+	@Deprecated
 	public byte[] surface2wkb(IomObject obj,boolean asCurvePolygon,double strokeP)
 	throws Iox2wkbException
 	{
@@ -613,6 +439,7 @@ public class Iox2wkb {
 		return os.toByteArray();
 	}
 
+	@Deprecated
 	public byte[] multisurface2wkb(IomObject obj,boolean asCurvePolygon,double strokeP)
 	throws Iox2wkbException
 	{
