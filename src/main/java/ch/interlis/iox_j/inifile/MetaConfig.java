58250c19b89a9938ddb67fa2e48bd70596e3fdf6
claeis <ce@eisenhutinformatik.ch>
2022-12-12 17:22:35 +0100

    API: add MetaConfig

---
+++ src/main/java/ch/interlis/iox_j/inifile/MetaConfig.java
@@ -0,0 +1,12 @@
+package ch.interlis.iox_j.inifile;
+
+public class MetaConfig {
+    public static final String CONFIGURATION = "CONFIGURATION";
+    public static final String CONFIG_VALIDATOR_CONFIG = "org.interlis2.validator.config";
+    public static final String CONFIG_REFERENCE_DATA = "ch.interlis.referenceData";
+    public static final String CONFIG_BASE_CONFIG = "baseConfig";
+
+    private MetaConfig() {
+    };
+
+}
