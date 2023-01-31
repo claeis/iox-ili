8ca99a80665f9a477dec567b2340ab49899250ce
claeis <ce@eisenhutinformatik.ch>
2023-01-31 09:49:53 +0100

    Validator: PlausibilityConstraint: add calculated value to log (claeis/ilivalidator#367)

--- src/main/java/ch/interlis/iox_j/validator/Validator.java
+++ src/main/java/ch/interlis/iox_j/validator/Validator.java
@@ -1048,7 +1048,8 @@ public class Validator implements ch.interlis.iox.IoxValidator {
 						msg = String.format("%s %s", msg, getDisplayName(constraint));
 					}
 
-					if(constraintEntry.getKey().getDirection()==0){ // >=
+					if(constraintEntry.getKey().getDirection()==PlausibilityConstraint.DIRECTION_AT_LEAST){ // >=
+                        errs.addEvent(errFact.logInfoMsg(rsrc.getString("validatePlausibilityConstraints.calculatedValue"),">=",Double.toString(((constraintEntry.getValue().getSuccessfulResults()/constraintEntry.getValue().getTotalSumOfConstraints())*100)),Double.toString(constraintEntry.getKey().getPercentage())));                        
 						if(((constraintEntry.getValue().getSuccessfulResults()/constraintEntry.getValue().getTotalSumOfConstraints())*100) >= constraintEntry.getKey().getPercentage()){
 							// ok
 						} else {
@@ -1058,7 +1059,8 @@ public class Validator implements ch.interlis.iox.IoxValidator {
 								logMsg(checkConstraint, rsrc.getString("validatePlausibilityConstraints.plausibilityConstraintIsNotTrue"), getDisplayName(constraintEntry.getKey()));
 							}
 						}
-					} else if(constraintEntry.getKey().getDirection()==1){ // <=
+					} else if(constraintEntry.getKey().getDirection()==PlausibilityConstraint.DIRECTION_AT_MOST){ // <=
+                        errs.addEvent(errFact.logInfoMsg(rsrc.getString("validatePlausibilityConstraints.calculatedValue"),"<=",Double.toString(((constraintEntry.getValue().getSuccessfulResults()/constraintEntry.getValue().getTotalSumOfConstraints())*100)),Double.toString(constraintEntry.getKey().getPercentage())));                        
 						if(((constraintEntry.getValue().getSuccessfulResults()/constraintEntry.getValue().getTotalSumOfConstraints())*100) <= constraintEntry.getKey().getPercentage()){
 							// ok
 						} else {
