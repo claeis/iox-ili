f1e9e8be54c3bda449884136d7aac0b316fca9b9
Dominic Burger <dominic.burger@geowerkstatt.ch>
2023-10-11 11:47:53 +0200

    Validate generic coords with concrete domain
    Co-authored-by: Patrick Ackermann <patrick.ackermann@geowerkstatt.ch>

--- src/main/java/ch/interlis/iox_j/validator/Validator.java
+++ src/main/java/ch/interlis/iox_j/validator/Validator.java
@@ -1,7 +1,6 @@
 package ch.interlis.iox_j.validator;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.StringWriter;
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -22,8 +21,6 @@ import ch.ehi.basics.logging.EhiLogger;
 import ch.ehi.basics.settings.Settings;
 import ch.ehi.basics.types.OutParam;
 import ch.ehi.iox.objpool.ObjectPoolManager;
-import ch.ehi.iox.objpool.impl.IomObjectSerializer;
-import ch.ehi.iox.objpool.impl.LongSerializer;
 import ch.interlis.ili2c.Ili2cException;
 import ch.interlis.ili2c.gui.UserSettings;
 import ch.interlis.ili2c.metamodel.AbstractClassDef;
@@ -207,7 +204,8 @@ public class Validator implements ch.interlis.iox.IoxValidator {
     private IoxWriter writer=null;
     private long objectCount=0l;
     private long structCount=0l;
-    
+	private Map<String, Domain> genericDomains = new HashMap<String, Domain>();
+
 	@Deprecated
 	public Validator(TransferDescription td, IoxValidationConfig validationConfig,
 			IoxLogging errs, LogEventFactory errFact, Settings config) {
@@ -376,7 +374,7 @@ public class Validator implements ch.interlis.iox.IoxValidator {
 			}
 		} else if (event instanceof ch.interlis.iox.StartBasketEvent){
 			StartBasketEvent startBasketEvent = ((ch.interlis.iox.StartBasketEvent) event);
-			currentBasketId = ((ch.interlis.iox.StartBasketEvent) event).getBid();
+			currentBasketId = startBasketEvent.getBid();
 			validateBasketEvent(startBasketEvent);
 		}else if(event instanceof ch.interlis.iox.ObjectEvent){
 			IomObject iomObj=new ch.interlis.iom_j.Iom_jObject(((ch.interlis.iox.ObjectEvent)event).getIomObject());
@@ -389,7 +387,7 @@ public class Validator implements ch.interlis.iox.IoxValidator {
 				throw e;
 			}
 		} else if (event instanceof ch.interlis.iox.EndBasketEvent){
-		    clearCurrentBid();
+		    cleanupCurrentBasket();
 		}else if (event instanceof ch.interlis.iox.EndTransferEvent){
 	        String additionalModels=this.validationConfig.getConfigValue(ValidationConfig.PARAMETER, ValidationConfig.ADDITIONAL_MODELS);
 	        if(additionalModels!=null){
@@ -449,9 +447,10 @@ public class Validator implements ch.interlis.iox.IoxValidator {
         return null;
     }
     
-    private void clearCurrentBid() {
+    private void cleanupCurrentBasket() {
         currentMainOid = null;
         errFact.setDataObj(null);
+        genericDomains = new HashMap<String, Domain>();
     }
     private void validateBasketEvent(ch.interlis.iox.StartBasketEvent event) {
         boolean isValid = true;
@@ -493,6 +492,15 @@ public class Validator implements ch.interlis.iox.IoxValidator {
                         stableBids.put(bid, event.getType());
                     }
             	}
+            }
+        }
+
+        for (Entry<String, String> genericDomain : ((ch.interlis.iox_j.StartBasketEvent)event).getDomains().entrySet()) {
+            Element concreteDomain = td.getElement(genericDomain.getValue());
+            if (concreteDomain instanceof Domain) {
+                genericDomains.put(genericDomain.getKey(), (Domain) concreteDomain);
+            } else {
+                errs.addEvent(errFact.logErrorMsg(rsrc.getString("validateBasketEvent.genericDomainNotFound"), genericDomain.getValue(), genericDomain.getKey()));
             }
         }
     }
@@ -3615,7 +3623,9 @@ public class Validator implements ch.interlis.iox.IoxValidator {
 				 validateGeometryType=defaultGeometryTypeValidation;
 			 }
 		 }
-		Type type0=attr.getDomain();
+		Type attrType = attr.getDomain();
+		Domain domain = attrType instanceof TypeAlias ? ((TypeAlias)attrType).getAliasing() : null;
+		Model model = (Model)attr.getContainer(Model.class);
 		Type type = attr.getDomainResolvingAll();
 		if (type instanceof CompositionType){
 			 int structc=iomObj.getattrvaluecount(attrName);
@@ -3847,7 +3857,7 @@ public class Validator implements ch.interlis.iox.IoxValidator {
                         PolylineType polylineType=(PolylineType)type;
                         IomObject polylineValue=iomObj.getattrobj(attrName, structi);
                         if (polylineValue != null){
-                            boolean isValid=validatePolyline(validateGeometryType, polylineType, polylineValue, attrName);
+                            boolean isValid=validatePolyline(validateGeometryType, model, polylineType, polylineValue, attrName);
                             if(isValid){
                                 validatePolylineTopology(attrPath,validateGeometryType, polylineType, polylineValue);
                             }
@@ -3866,7 +3876,7 @@ public class Validator implements ch.interlis.iox.IoxValidator {
                                 if(polylinec>0) {
                                     for(int polylinei=0;polylinei<polylinec;polylinei++) {
                                         IomObject polylineValue=multipolylineValue.getattrobj("polyline", polylinei);
-                                        boolean isValid=validatePolyline(validateGeometryType, polylineType, polylineValue, attrName);
+                                        boolean isValid=validatePolyline(validateGeometryType, model, polylineType, polylineValue, attrName);
                                         if(isValid){
                                             validatePolylineTopology(attrPath,validateGeometryType, polylineType, polylineValue);
                                         }
@@ -3896,7 +3906,7 @@ public class Validator implements ch.interlis.iox.IoxValidator {
                             SurfaceOrAreaType surfaceOrAreaType=(SurfaceOrAreaType)type;
                             IomObject surfaceValue=iomObj.getattrobj(attrName,structi);
                             if (surfaceValue != null){
-                                boolean isValid = validatePolygon(validateGeometryType,surfaceOrAreaType, surfaceValue, iomObj, attrName);
+                                boolean isValid = validatePolygon(validateGeometryType, model, surfaceOrAreaType, surfaceValue, iomObj, attrName);
                                 if(isValid){
                                     Object attrValidator=pipelinePool.getIntermediateValue(attr, ValidationConfig.TOPOLOGY);
                                     if(attrValidator==null){
@@ -3941,7 +3951,7 @@ public class Validator implements ch.interlis.iox.IoxValidator {
                         MultiSurfaceOrAreaType surfaceOrAreaType=(MultiSurfaceOrAreaType)type;
                        IomObject surfaceValue=iomObj.getattrobj(attrName,structi);
                        if (surfaceValue != null){
-                           boolean isValid = validatePolygon(validateGeometryType,surfaceOrAreaType, surfaceValue, iomObj, attrName);
+                           boolean isValid = validatePolygon(validateGeometryType, model, surfaceOrAreaType, surfaceValue, iomObj, attrName);
                            if(isValid){
                                Object attrValidator=pipelinePool.getIntermediateValue(attr, ValidationConfig.TOPOLOGY);
                                if(attrValidator==null){
@@ -3983,7 +3993,7 @@ public class Validator implements ch.interlis.iox.IoxValidator {
                     }else if(type instanceof CoordType){
                         IomObject coord=iomObj.getattrobj(attrName, structi);
                         if (coord!=null){
-                            validateCoordType(validateGeometryType, (CoordType)type, coord, attrName);
+                            validateCoordType(validateGeometryType, model, domain, (CoordType)type, coord, attrName);
                         } else {
                             String attrValue=iomObj.getattrprim(attrName, structi);
                             if (attrValue != null) {
@@ -3999,7 +4009,7 @@ public class Validator implements ch.interlis.iox.IoxValidator {
                                     for(int coordi=0;coordi<coordc;coordi++) {
                                         IomObject coordValue=multicoordValue.getattrobj("coord", coordi);
                                         if(coordValue.getobjecttag().equals("COORD")) {
-                                            validateCoordType(validateGeometryType, (MultiCoordType)type, coordValue, attrName);
+                                            validateCoordType(validateGeometryType, model, domain, (MultiCoordType)type, coordValue, attrName);
                                         }else {
                                             logMsg(validateType, "unexpected Type "+coordValue.getobjecttag()+"; COORD expected");
                                         }
@@ -4242,7 +4252,7 @@ public class Validator implements ch.interlis.iox.IoxValidator {
 	/* returns true, if polygon is valid
 	 * 
 	 */
-	private boolean validatePolygon(String validateType, AbstractSurfaceOrAreaType surfaceOrAreaType, IomObject surfaceValue, IomObject currentIomObj, String attrName) {
+	private boolean validatePolygon(String validateType, Model model, AbstractSurfaceOrAreaType surfaceOrAreaType, IomObject surfaceValue, IomObject currentIomObj, String attrName) {
         boolean foundErrs=false;
 		if (surfaceValue.getobjecttag().equals("MULTISURFACE")){
 		    int surfacec=surfaceValue.getattrvaluecount("surface");
@@ -4277,7 +4287,7 @@ public class Validator implements ch.interlis.iox.IoxValidator {
 						}    
 						for(int polylinei=0;polylinei<boundary.getattrvaluecount("polyline");polylinei++){
 							IomObject polyline=boundary.getattrobj("polyline",polylinei);
-					        foundErrs = foundErrs || !validatePolyline(validateType, surfaceOrAreaType, polyline, attrName);
+					        foundErrs = foundErrs || !validatePolyline(validateType, model, surfaceOrAreaType, polyline, attrName);
 							// add line to shell or hole
 						}
 					    // add shell or hole to surface
@@ -4292,7 +4302,7 @@ public class Validator implements ch.interlis.iox.IoxValidator {
 	}
 
 	// returns true if valid
-	private boolean validatePolyline(String validateType, LineType polylineType, IomObject polylineValue, String attrName) {
+	private boolean validatePolyline(String validateType, Model model, LineType polylineType, IomObject polylineValue, String attrName) {
 		boolean foundErrs=false;
 		if (polylineValue.getobjecttag().equals("POLYLINE")){
 			boolean clipped = polylineValue.getobjectconsistency()==IomConstants.IOM_INCOMPLETE;
@@ -4318,7 +4328,8 @@ public class Validator implements ch.interlis.iox.IoxValidator {
 						IomObject segment=sequence.getattrobj("segment",segmenti);
 						if(segment.getobjecttag().equals("COORD")){
 							if(lineformNames.contains("STRAIGHTS") || segmenti==0){
-							    foundErrs = foundErrs || !validateCoordType(validateType, (CoordType) polylineType.getControlPointDomain().getType(), segment, attrName);
+								Domain coordDomain = polylineType.getControlPointDomain();
+							    foundErrs = foundErrs || !validateCoordType(validateType, model, coordDomain, (CoordType) coordDomain.getType(), segment, attrName);
 							}else{
 								logMsg(validateType, "unexpected COORD");
 								foundErrs = foundErrs || true;
@@ -4347,10 +4358,47 @@ public class Validator implements ch.interlis.iox.IoxValidator {
 		return !foundErrs;
 	}
 
+	private AbstractCoordType resolveGenericCoordType(String validateType, Model model, Domain coordDomain) {
+		// resolve generic domain from contexts in model
+		Domain[] resolved = model.resolveGenericDomain(coordDomain);
+		if (resolved == null) {
+			logMsg(validateType, rsrc.getString("validateCoordType.missingContext"), coordDomain.toString());
+			return null;
+		}
+
+		// search deferred generic in transfer
+		Domain concreteDomain = genericDomains.get(coordDomain.getScopedName());
+		if (concreteDomain != null) {
+			for (Domain domain : resolved) {
+				if (domain == concreteDomain) {
+					return (AbstractCoordType) concreteDomain.getType();
+				}
+			}
+
+			logMsg(validateType, rsrc.getString("validateCoordType.invalidDomain"), concreteDomain.toString(), coordDomain.toString());
+			return null;
+		}
+
+		if (resolved.length == 1) {
+			return (AbstractCoordType) resolved[0].getType();
+		}
+
+		logMsg(validateType, rsrc.getString("validateCoordType.multipleDomainsInContext"), coordDomain.toString());
+		return null;
+    }
+
     // returns true if valid
-	private boolean validateCoordType(String validateType, AbstractCoordType coordType, IomObject coordValue, String attrName) {
+	private boolean validateCoordType(String validateType, Model model, Domain coordDomain, AbstractCoordType coordType, IomObject coordValue, String attrName) {
         boolean foundErrs=false;
         OutParam<Boolean> isNumValid=new OutParam<Boolean>(true);
+
+		if (coordType.isGeneric()) {
+			coordType = resolveGenericCoordType(validateType, model, coordDomain);
+			if (coordType == null) {
+				return false;
+			}
+		}
+
 		if (coordType.getDimensions().length >= 1){
 			if (coordValue.getattrvalue("C1") != null){
 				coordValue.setattrvalue("C1", validateNumericType(validateType, (NumericType)coordType.getDimensions()[0], coordValue.getattrvalue("C1"), attrName,isNumValid));
---
+++ src/test/java/ch/interlis/iox_j/validator/GenericValueRanges24Test.java
@@ -0,0 +1,185 @@
+package ch.interlis.iox_j.validator;
+
+import ch.ehi.basics.settings.Settings;
+import ch.interlis.ili2c.Ili2cFailure;
+import ch.interlis.ili2c.config.Configuration;
+import ch.interlis.ili2c.config.FileEntry;
+import ch.interlis.ili2c.config.FileEntryKind;
+import ch.interlis.ili2c.metamodel.TransferDescription;
+import ch.interlis.iom.IomObject;
+import ch.interlis.iom_j.Iom_jObject;
+import ch.interlis.iox_j.*;
+import ch.interlis.iox_j.logging.LogEventFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class GenericValueRanges24Test {
+
+    private static final String DATA_DIRECTORY = "src/test/data/validator/GenericValueRanges/";
+
+    private TransferDescription compileIli(String filename) {
+        Configuration ili2cConfig = new Configuration();
+        FileEntry fileEntry = new FileEntry(filename, FileEntryKind.ILIMODELFILE);
+        ili2cConfig.addFileEntry(fileEntry);
+        TransferDescription td = null;
+        try {
+            td = ch.interlis.ili2c.Ili2c.runCompiler(ili2cConfig);
+        } catch (Ili2cFailure e) {
+            Assert.fail("Could not compile ili file <" + filename + ">: " + e.getMessage());
+        }
+        assertNotNull(td);
+
+        return td;
+    }
+
+    private IomObject createTestClassIomObjectLv03(String tag) {
+        IomObject iomObj = new Iom_jObject(tag, "o1");
+        iomObj.addattrobj("attr1", IomObjectHelper.createCoord("460002.0", "45002.0"));
+
+        return iomObj;
+    }
+
+    @Test
+    public void missingContext() {
+        TransferDescription td = compileIli(DATA_DIRECTORY + "MissingContext24.ili");
+
+        ValidationConfig modelConfig = new ValidationConfig();
+        LogCollector logger = new LogCollector();
+        LogEventFactory errFactory = new LogEventFactory();
+        Settings settings = new Settings();
+        Validator validator = new Validator(td, modelConfig, logger, errFactory, settings);
+        validator.validate(new StartTransferEvent());
+        validator.validate(new StartBasketEvent("ModelA.TestA", "b1"));
+        validator.validate(new ObjectEvent(createTestClassIomObjectLv03("ModelA.TestA.ClassA")));
+        validator.validate(new EndBasketEvent());
+        validator.validate(new EndTransferEvent());
+
+        assertEquals(1, logger.getErrs().size());
+        assertEquals("The Context for DOMAIN ModelA.Coord is missing", logger.getErrs().get(0).getEventMsg());
+    }
+
+    @Test
+    public void nonExistentDomainInBasket() {
+        TransferDescription td = compileIli(DATA_DIRECTORY + "GenericValueRanges24.ili");
+        HashMap<String, String> genericDomains = new HashMap<String, String>();
+        genericDomains.put("MultiCrs24.Coord", "notExistingDomain");
+
+        ValidationConfig modelConfig = new ValidationConfig();
+        LogCollector logger = new LogCollector();
+        LogEventFactory errFactory = new LogEventFactory();
+        Settings settings = new Settings();
+        Validator validator = new Validator(td, modelConfig, logger, errFactory, settings);
+        validator.validate(new StartTransferEvent());
+        validator.validate(new StartBasketEvent("MultiCrs24.TestA", "b1", genericDomains));
+        validator.validate(new EndBasketEvent());
+        validator.validate(new EndTransferEvent());
+
+        assertEquals(1, logger.getErrs().size());
+        assertEquals("Concrete domain notExistingDomain of generic domain MultiCrs24.Coord not found", logger.getErrs().get(0).getEventMsg());
+    }
+
+    @Test
+    public void invalidDomainInBasket() {
+        TransferDescription td = compileIli(DATA_DIRECTORY + "GenericValueRanges24.ili");
+        HashMap<String, String> genericDomains = new HashMap<String, String>();
+        genericDomains.put("MultiCrs24.Coord", "MultiCrs24.Coord_Other");
+
+        ValidationConfig modelConfig = new ValidationConfig();
+        LogCollector logger = new LogCollector();
+        LogEventFactory errFactory = new LogEventFactory();
+        Settings settings = new Settings();
+        Validator validator = new Validator(td, modelConfig, logger, errFactory, settings);
+        validator.validate(new StartTransferEvent());
+        validator.validate(new StartBasketEvent("MultiCrs24.TestA", "b1", genericDomains));
+        validator.validate(new ObjectEvent(createTestClassIomObjectLv03("MultiCrs24.TestA.ClassA")));
+        validator.validate(new EndBasketEvent());
+        validator.validate(new EndTransferEvent());
+
+        assertEquals(1, logger.getErrs().size());
+        assertEquals("The concrete DOMAIN MultiCrs24.Coord_Other is not valid for generic DOMAIN MultiCrs24.Coord", logger.getErrs().get(0).getEventMsg());
+    }
+
+    @Test
+    public void domainInBasket() {
+        TransferDescription td = compileIli(DATA_DIRECTORY + "GenericValueRanges24.ili");
+        HashMap<String, String> genericDomains = new HashMap<String, String>();
+        genericDomains.put("MultiCrs24.Coord", "MultiCrs24.Coord_LV03");
+
+        ValidationConfig modelConfig = new ValidationConfig();
+        LogCollector logger = new LogCollector();
+        LogEventFactory errFactory = new LogEventFactory();
+        Settings settings = new Settings();
+        Validator validator = new Validator(td, modelConfig, logger, errFactory, settings);
+        validator.validate(new StartTransferEvent());
+        validator.validate(new StartBasketEvent("MultiCrs24.TestA", "b1", genericDomains));
+        validator.validate(new ObjectEvent(createTestClassIomObjectLv03("MultiCrs24.TestA.ClassA")));
+        validator.validate(new EndBasketEvent());
+        validator.validate(new EndTransferEvent());
+
+        assertEquals(0, logger.getErrs().size());
+    }
+
+    @Test
+    public void oneDomainInContext() {
+        TransferDescription td = compileIli(DATA_DIRECTORY + "GenericValueRangesMultipleModels24.ili");
+
+        ValidationConfig modelConfig = new ValidationConfig();
+        LogCollector logger = new LogCollector();
+        LogEventFactory errFactory = new LogEventFactory();
+        Settings settings = new Settings();
+        Validator validator = new Validator(td, modelConfig, logger, errFactory, settings);
+        validator.validate(new StartTransferEvent());
+        validator.validate(new StartBasketEvent("ModelB.TestB", "b1"));
+        validator.validate(new ObjectEvent(createTestClassIomObjectLv03("ModelB.TestB.ClassB")));
+        validator.validate(new EndBasketEvent());
+        validator.validate(new EndTransferEvent());
+
+        assertEquals(0, logger.getErrs().size());
+    }
+
+    @Test
+    public void deferredDomainMissingInTransfer() {
+        TransferDescription td = compileIli(DATA_DIRECTORY + "GenericValueRanges24.ili");
+
+        ValidationConfig modelConfig = new ValidationConfig();
+        LogCollector logger = new LogCollector();
+        LogEventFactory errFactory = new LogEventFactory();
+        Settings settings = new Settings();
+        Validator validator = new Validator(td, modelConfig, logger, errFactory, settings);
+        validator.validate(new StartTransferEvent());
+        validator.validate(new StartBasketEvent("MultiCrs24.TestA", "b1"));
+        validator.validate(new ObjectEvent(createTestClassIomObjectLv03("MultiCrs24.TestA.ClassA")));
+        validator.validate(new EndBasketEvent());
+        validator.validate(new EndTransferEvent());
+
+        assertEquals(1, logger.getErrs().size());
+        assertEquals("Could not choose between multiple concrete domains for generic DOMAIN MultiCrs24.Coord", logger.getErrs().get(0).getEventMsg());
+    }
+
+    @Test
+    public void wrongObjectCoordinates() {
+        TransferDescription td = compileIli(DATA_DIRECTORY + "GenericValueRanges24.ili");
+        HashMap<String, String> genericDomains = new HashMap<String, String>();
+        genericDomains.put("MultiCrs24.Coord", "MultiCrs24.Coord_LV95");
+
+        ValidationConfig modelConfig = new ValidationConfig();
+        LogCollector logger = new LogCollector();
+        LogEventFactory errFactory = new LogEventFactory();
+        Settings settings = new Settings();
+        Validator validator = new Validator(td, modelConfig, logger, errFactory, settings);
+        validator.validate(new StartTransferEvent());
+        validator.validate(new StartBasketEvent("MultiCrs24.TestA", "b1", genericDomains));
+        validator.validate(new ObjectEvent(createTestClassIomObjectLv03("MultiCrs24.TestA.ClassA")));
+        validator.validate(new EndBasketEvent());
+        validator.validate(new EndTransferEvent());
+
+        assertEquals(2, logger.getErrs().size());
+        LogCollectorAssertions.AssertContainsError("value 460002.000 is out of range in attribute attr1", 1, logger);
+        LogCollectorAssertions.AssertContainsError("value 45002.000 is out of range in attribute attr1", 1, logger);
+    }
+}
