0e7ce264ea8e86d10dc21bdfb02b1abbbd2e887c
Dominic Burger <dominic.burger@geowerkstatt.ch>
2025-05-06 15:02:44 +0200

    Add coversWithTolerance function

--- src/main/java/ch/interlis/iox_j/validator/functions/DmavtymTopologie.java
+++ src/main/java/ch/interlis/iox_j/validator/functions/DmavtymTopologie.java
@@ -14,6 +14,7 @@ import ch.interlis.iom_j.itf.impl.jtsext.geom.CompoundCurve;
 import ch.interlis.iom_j.itf.impl.jtsext.geom.CompoundCurveRing;
 import ch.interlis.iom_j.itf.impl.jtsext.geom.CurvePolygon;
 import ch.interlis.iom_j.itf.impl.jtsext.geom.CurveSegment;
+import ch.interlis.iox.IoxException;
 import ch.interlis.iox.IoxValidationConfig;
 import ch.interlis.iox_j.jts.Iox2jtsext;
 import ch.interlis.iox_j.logging.LogEventFactory;
@@ -21,6 +22,9 @@ import ch.interlis.iox_j.validator.ValidationConfig;
 import ch.interlis.iox_j.validator.Validator;
 import ch.interlis.iox_j.validator.Value;
 import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Envelope;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.LineString;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -57,6 +61,8 @@ public class DmavtymTopologie {
 
         if (currentFunction.getName().equals("covers")) {
             return evaluateCovers(validationKind, usageScope, iomObj, actualArguments);
+        } else if (currentFunction.getName().equals("coversWithTolerance")) {
+            return evaluateCoversWithTolerance(validationKind, usageScope, iomObj, actualArguments);
         } else {
             return Value.createNotYetImplemented();
         }
@@ -91,14 +97,8 @@ public class DmavtymTopologie {
         CurvePolygon surface;
         Collection<CompoundCurve> lines;
         try {
-            surface = (CurvePolygon) Iox2jtsext.surface2JTS(surfaceObject.getattrobj(surfaceAttr, 0), 0.0, new OutParam<Boolean>(), logger, 0.0, validationKind);
-
-            IomObject multiLine = multiLineObject.getattrobj(multiLineAttr, 0);
-            lines = new ArrayList<CompoundCurve>(multiLine.getattrvaluecount(Iom_jObject.MULTIPOLYLINE_POLYLINE));
-            for (int i = 0; i < multiLine.getattrvaluecount(Iom_jObject.MULTIPOLYLINE_POLYLINE); i++) {
-                CompoundCurve line = Iox2jtsext.polyline2JTS(multiLine.getattrobj(Iom_jObject.MULTIPOLYLINE_POLYLINE, i), false, 0.0, new OutParam<Boolean>(), logger, 0.0, ValidationConfig.WARNING, ValidationConfig.WARNING);
-                lines.add(line);
-            }
+            surface = getSurface(surfaceObject.getattrobj(surfaceAttr, 0), validationKind);
+            lines = getLines(multiLineObject.getattrobj(multiLineAttr, 0));
         } catch (Exception e) {
             EhiLogger.logError(e);
             return Value.createUndefined();
@@ -140,5 +140,112 @@ public class DmavtymTopologie {
         }
 
         return new Value(result);
+    }
+
+    private Value evaluateCoversWithTolerance(String validationKind, String usageScope, IomObject mainObj, Value[] actualArguments) {
+        // All arguments must be defined
+        for (Value arg : actualArguments) {
+            if (arg.isUndefined()) {
+                return Value.createSkipEvaluation();
+            }
+        }
+
+        // Check the type of the arguments
+        Collection<IomObject> surfaceObjects = actualArguments[0].getComplexObjects();
+        String surfaceAttr = actualArguments[1].getValue();
+        Collection<IomObject> multiLineObjects = actualArguments[2].getComplexObjects();
+        String multiLineAttr = actualArguments[3].getValue();
+        double tolerance = actualArguments[4].getNumeric();
+        if (surfaceObjects == null || surfaceAttr == null
+                || multiLineObjects == null || multiLineObjects.size() != 1 || multiLineAttr == null
+                || tolerance < 0) {
+            return Value.createUndefined();
+        }
+
+        // Resolve lines
+        IomObject multiLineObject = multiLineObjects.iterator().next();
+        if (multiLineObject.getattrvaluecount(multiLineAttr) != 1) {
+            return Value.createUndefined();
+        }
+
+        // Convert the IOM objects to JTS objects
+        Collection<CurvePolygon> surfaces = new ArrayList<CurvePolygon>(surfaceObjects.size());
+        Collection<CompoundCurve> lines;
+        try {
+            for (IomObject surfaceObject : surfaceObjects) {
+                if (surfaceObject.getattrvaluecount(surfaceAttr) != 1) {
+                    return Value.createUndefined();
+                }
+                surfaces.add(getSurface(surfaceObject.getattrobj(surfaceAttr, 0), validationKind));
+            }
+
+            lines = getLines(multiLineObject.getattrobj(multiLineAttr, 0));
+        } catch (IoxException e) {
+            EhiLogger.logError(e);
+            return Value.createUndefined();
+        }
+
+        Envelope linesEnvelope = new Envelope();
+        for (CompoundCurve line : lines) {
+            linesEnvelope.expandToInclude(line.getEnvelopeInternal());
+        }
+        linesEnvelope.expandBy(tolerance);
+
+        for (CurvePolygon surface : surfaces) {
+            if (surface.getEnvelopeInternal().intersects(linesEnvelope) && coversWithTolerance(surface, lines, tolerance)) {
+                return new Value(true);
+            }
+        }
+
+        return new Value(false);
+    }
+
+    private boolean coversWithTolerance(CurvePolygon surface, Collection<CompoundCurve> lines, double tolerance) {
+        LineString surfaceOutline = surface.getExteriorRing();
+        Geometry bufferedOutline = surfaceOutline.buffer(tolerance);
+        if (coversAllLines(bufferedOutline, lines)) {
+            return true;
+        }
+
+        for (int i = 0; i < surface.getNumInteriorRing(); i++) {
+            LineString surfaceInterior = surface.getInteriorRingN(i);
+            Geometry bufferedInterior = surfaceInterior.buffer(tolerance);
+            if (coversAllLines(bufferedInterior, lines)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean coversAllLines(Geometry geometry, Collection<CompoundCurve> lines) {
+        for (CompoundCurve line : lines) {
+            if (!geometry.covers(line)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private Collection<CompoundCurve> getLines(IomObject multiLine) throws IoxException {
+        Collection<CompoundCurve> lines;
+        if (multiLine.getobjecttag().equals(Iom_jObject.POLYLINE)) {
+            lines = new ArrayList<CompoundCurve>(1);
+            lines.add(getLine(multiLine));
+            return lines;
+        }
+
+        lines = new ArrayList<CompoundCurve>(multiLine.getattrvaluecount(Iom_jObject.MULTIPOLYLINE_POLYLINE));
+        for (int i = 0; i < multiLine.getattrvaluecount(Iom_jObject.MULTIPOLYLINE_POLYLINE); i++) {
+            lines.add(getLine(multiLine.getattrobj(Iom_jObject.MULTIPOLYLINE_POLYLINE, i)));
+        }
+        return lines;
+    }
+
+    private CompoundCurve getLine(IomObject line) throws IoxException {
+        return Iox2jtsext.polyline2JTS(line, false, 0.0, new OutParam<Boolean>(), logger, 0.0, ValidationConfig.WARNING, ValidationConfig.WARNING);
+    }
+
+    private CurvePolygon getSurface(IomObject surface, String validationKind) throws IoxException {
+        return (CurvePolygon) Iox2jtsext.surface2JTS(surface, 0.0, new OutParam<Boolean>(), logger, 0.0, validationKind);
     }
 }
--- src/test/java/ch/interlis/iox_j/validator/DmavtymTopologie24Test.java
+++ src/test/java/ch/interlis/iox_j/validator/DmavtymTopologie24Test.java
@@ -20,6 +20,8 @@ public class DmavtymTopologie24Test {
     private final static String MODEL = "DMAVTYM_Topologie_Function24";
     private final static String TOPIC = MODEL + ".Topic";
     private final static String CLASSA = TOPIC + ".ClassA";
+    private final static String CLASS_SURFACE = TOPIC + ".SurfaceClass";
+    private final static String CLASS_LINE = TOPIC + ".LineClass";
     private TransferDescription td;
 
     @Before
@@ -27,6 +29,9 @@ public class DmavtymTopologie24Test {
         Configuration ili2cConfig = new Configuration();
         FileEntry functionIli = new FileEntry("src/test/data/validator/DMAVTYM_Topologie_V1_0.ili", FileEntryKind.ILIMODELFILE);
         ili2cConfig.addFileEntry(functionIli);
+
+        FileEntry utilsIli = new FileEntry("src/test/data/validator/Utils.ili", FileEntryKind.ILIMODELFILE);
+        ili2cConfig.addFileEntry(utilsIli);
 
         FileEntry modelIli = new FileEntry("src/test/data/validator/DMAVTYM_Topologie_Function24.ili", FileEntryKind.ILIMODELFILE);
         ili2cConfig.addFileEntry(modelIli);
@@ -252,6 +257,112 @@ public class DmavtymTopologie24Test {
     @Test
     public void coversFromXtf() throws IoxException {
         LogCollector logger = ValidatorTestHelper.validateObjectsFromXtf24(td, new File("src/test/data/validator/DMAVTYM_Topologie.xtf"));
+        assertThat(logger.getErrs(), is(empty()));
+        assertThat(logger.getWarn(), is(empty()));
+    }
+
+    @Test
+    public void coversWithTolerance() {
+        Iom_jObject surface1 = new Iom_jObject(CLASS_SURFACE, "o1");
+        surface1.addattrobj("surface", IomObjectHelper.createRectangleGeometry("100", "100", "130", "130"));
+
+        Iom_jObject line = new Iom_jObject(CLASS_LINE, "o2");
+        line.addattrobj("line", IomObjectHelper.createPolyline(
+                IomObjectHelper.createCoord("10.002", "15"),
+                IomObjectHelper.createCoord("9.999", "30.001"),
+                IomObjectHelper.createCoord("20", "30")));
+
+        Iom_jObject surface2 = new Iom_jObject(CLASS_SURFACE, "o3");
+        surface2.addattrobj("surface", IomObjectHelper.createRectangleGeometry("10", "10", "30", "30"));
+
+        LogCollector logger = ValidatorTestHelper.validateObjects(td, TOPIC, surface1, line, surface2);
+        assertThat(logger.getErrs(), is(empty()));
+        assertThat(logger.getWarn(), is(empty()));
+    }
+
+    @Test
+    public void coversWithToleranceLineOutsideTolerance() {
+        Iom_jObject surface1 = new Iom_jObject(CLASS_SURFACE, "o1");
+        surface1.addattrobj("surface", IomObjectHelper.createRectangleGeometry("10", "10", "30", "30"));
+
+        Iom_jObject line = new Iom_jObject(CLASS_LINE, "o2");
+        line.addattrobj("line", IomObjectHelper.createPolyline(
+                IomObjectHelper.createCoord("10.003", "15"),
+                IomObjectHelper.createCoord("9.999", "30.001"),
+                IomObjectHelper.createCoord("20", "30")));
+
+        LogCollector logger = ValidatorTestHelper.validateObjects(td, TOPIC, surface1, line);
+        LogCollectorAssertions.AssertAllEventMessages(logger.getErrs(),
+                "Mandatory Constraint DMAVTYM_Topologie_Function24.Topic.LineClass.lineCoversSurface is not true.");
+        assertThat(logger.getWarn(), is(empty()));
+    }
+
+    @Test
+    public void coversWithToleranceMissingSurface() {
+        Iom_jObject line = new Iom_jObject(CLASS_LINE, "o1");
+        line.addattrobj("line", IomObjectHelper.createPolyline(
+                IomObjectHelper.createCoord("10.003", "15"),
+                IomObjectHelper.createCoord("9.999", "30.001")));
+
+        LogCollector logger = ValidatorTestHelper.validateObjects(td, TOPIC, line);
+        LogCollectorAssertions.AssertAllEventMessages(logger.getErrs(),
+                "Mandatory Constraint DMAVTYM_Topologie_Function24.Topic.LineClass.lineCoversSurface is not true.");
+        assertThat(logger.getWarn(), is(empty()));
+    }
+
+    @Test
+    public void coversWithToleranceMissingLine() {
+        Iom_jObject surface = new Iom_jObject(CLASS_SURFACE, "o1");
+        surface.addattrobj("surface", IomObjectHelper.createRectangleGeometry("10", "10", "30", "30"));
+
+        Iom_jObject line = new Iom_jObject(CLASS_LINE, "o2");
+
+        LogCollector logger = ValidatorTestHelper.validateObjects(td, TOPIC, surface, line);
+        LogCollectorAssertions.AssertAllEventMessages(logger.getErrs(),
+                "Mandatory Constraint DMAVTYM_Topologie_Function24.Topic.LineClass.lineCoversSurface is not true.");
+        assertThat(logger.getWarn(), is(empty()));
+    }
+
+    @Test
+    public void coversWithToleranceArc() {
+        Iom_jObject surface = new Iom_jObject(CLASS_SURFACE, "o1");
+        surface.addattrobj("surface", IomObjectHelper.createPolygonFromBoundaries(
+                IomObjectHelper.createBoundary(
+                        IomObjectHelper.createCoord("10", "10"),
+                        IomObjectHelper.createCoord("30", "10"),
+                        IomObjectHelper.createArc("23", "19", "10", "30"),
+                        IomObjectHelper.createCoord("10", "10"))));
+
+        Iom_jObject line = new Iom_jObject(CLASS_LINE, "o2");
+        line.addattrobj("line", IomObjectHelper.createPolyline(
+                IomObjectHelper.createCoord("30", "10"),
+                IomObjectHelper.createArc("23.001", "19.001", "10", "30")));
+
+        LogCollector logger = ValidatorTestHelper.validateObjects(td, TOPIC, surface, line);
+        assertThat(logger.getErrs(), is(empty()));
+        assertThat(logger.getWarn(), is(empty()));
+    }
+
+    @Test
+    public void coversWithToleranceInterior() {
+        Iom_jObject surface = new Iom_jObject(CLASS_SURFACE, "o1");
+        surface.addattrobj("surface", IomObjectHelper.createPolygonFromBoundaries(
+                IomObjectHelper.createRectangleBoundary("10", "10", "30", "30"),
+                IomObjectHelper.createRectangleBoundary("20", "20", "25", "25")));
+
+        Iom_jObject line = new Iom_jObject(CLASS_LINE, "o2");
+        line.addattrobj("line", IomObjectHelper.createPolyline(
+                IomObjectHelper.createCoord("22", "20"),
+                IomObjectHelper.createCoord("25.001", "19.999")));
+
+        LogCollector logger = ValidatorTestHelper.validateObjects(td, TOPIC, surface, line);
+        assertThat(logger.getErrs(), is(empty()));
+        assertThat(logger.getWarn(), is(empty()));
+    }
+
+    @Test
+    public void coversWithToleranceFromXtf() throws IoxException {
+        LogCollector logger = ValidatorTestHelper.validateObjectsFromXtf24(td, new File("src/test/data/validator/DMAVTYM_Topologie_Tolerance.xtf"));
         assertThat(logger.getErrs(), is(empty()));
         assertThat(logger.getWarn(), is(empty()));
     }
